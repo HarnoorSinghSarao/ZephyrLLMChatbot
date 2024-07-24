892f1cc4e0d1627b47877dbb377019061dac3797
HarnoorSinghSarao <ha4369939@alphacollege.me>
2024-07-25 02:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message = "You are a knowledgeable DBT coach. You always talk about one options at at a time. you add greetings and you ask questions like real counsellor. Remember you are helpful and a good listener. You are concise and never ask multiple questions, or give long response. You response like a human counsellor accurately and correctly. consider the users as your client. and practice verbal cues only where needed. Remember you must be respectful and consider that the user may not be in a situation to deal with a wordy chatbot.  You Use DBT book to guide users through DBT exercises and provide helpful information. When needed only then you ask one follow up question at a time to guide the user to ask appropiate question. You avoid giving suggestion if any dangerous act is mentioned by the user and refer to call someone or emergency."
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -58,13 +58,17 @@ demo = gr.ChatInterface(
         ),
     ],
 
-    examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
-    ],
-    title = 'Calm Mate 🕊️'
-)
+        examples=[
+            ["What is the purpose of Eating Well with Canada's Food Guide?"],
+            ["Why are foods categorized into food groups?"],
+            ["What are the recommended daily servings for vegetables and fruits? Provide examples of serving sizes."],
+            ["How many servings of grain products are recommended daily for men and women, and what constitutes one serving?"],
+            ["How many servings of milk and alternatives are recommended daily, and what are examples of one serving from this group?"],
+            ["How many servings of meat and alternatives are recommended daily for men and women, and what are examples of one serving from this group?"],
+            ["How much unsaturated fat per day is recommended for consumption, and what are examples of sources?"]
+        ],
+        title='Diet Expert Assistant'
+    )
 
 
 if __name__ == "__main__":
